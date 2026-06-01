665b387347cc5cb695e3b59694903fb712e577cc
Nivedita <niveditasinghrajput801@gmail.com>
2026-06-01 15:55:24 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -1,29 +1,43 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import analyzeRoute from "./routes/Analyze.js";
-
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", analyzeRoute);
-
-// app.listen(port, () => {
-//   console.log("Server running ");
-// });
-
-
-// Local development only
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 5000;
-
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.get("/", (req, res) => {
+  res.json({
+    status: "working"
   });
-}
+});
+
+export default app;
+
+
+
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// import analyzeRoute from "./routes/Analyze.js";
+
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api", analyzeRoute);
+
+// // app.listen(port, () => {
+// //   console.log("Server running ");
+// // });
+
+
+// // Local development only
+// if (process.env.NODE_ENV !== "production") {
+//   const port = process.env.PORT || 5000;
+
+//   app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+//   });
+// }
